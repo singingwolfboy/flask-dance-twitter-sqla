b5d511b52a1c33e5e3b78932185cacd656f7781e
David Baumgold <david@davidbaumgold.com>
2019-01-13 21:06:10 +0100

    oops again

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersekrit'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TWITTER_OAUTH_APP_KEY = os.environ.get('TWITTER_OAUTH_APP_KEY')
-    TWITTER_OAUTH_APP_SECRET = os.environ.get('TWITTER_OAUTH_APP_SECRET')
+    TWITTER_OAUTH_API_KEY = os.environ.get('TWITTER_OAUTH_API_KEY')
+    TWITTER_OAUTH_API_SECRET = os.environ.get('TWITTER_OAUTH_API_SECRET')
