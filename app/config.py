0c38e83c91b4ae473e5a7c5c164a0c2e62b55e42
David Baumgold <david@davidbaumgold.com>
2019-06-04 14:22:18 +0200

    dotenv plus FLASK_SECRET_KEY

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersekrit'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.sqlite3'
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "supersekrit"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///app.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TWITTER_OAUTH_CLIENT_KEY = os.environ.get('TWITTER_OAUTH_CLIENT_KEY')
-    TWITTER_OAUTH_CLIENT_SECRET = os.environ.get('TWITTER_OAUTH_CLIENT_SECRET')
+    TWITTER_OAUTH_CLIENT_KEY = os.environ.get("TWITTER_OAUTH_CLIENT_KEY")
+    TWITTER_OAUTH_CLIENT_SECRET = os.environ.get("TWITTER_OAUTH_CLIENT_SECRET")
